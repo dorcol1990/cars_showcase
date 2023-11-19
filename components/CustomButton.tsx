fc467f03488ec280f8385636f713760ebc2e3f68
dorcol1990 <tickets1312@yahoo.com>
2023-11-19 22:46:10 +0100

    add navbar, footer and filters

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+"use client";
 
-const CustomButton = () => {
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
+
+const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <button
+     disabled={false}
+     type={btnType || 'button'}
+     className={`custom-btn ${containerStyles}`}
+     onClick={handleClick}
+    >
+        <span className={`flex-1`}>
+            {title}
+        </span>
+    </button>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import { CustomButton } from '.';
 import Image from 'next/image'
 
 const Hero = () => {
+
+const handleScroll = ()=>{
+
+}
+
   return (
     <div className='hero'>
         <div className='flex-1 pt-36 padding-x'>
             <h1 className='hero__title'>Find, Book or Rent a Car - Quickly and Easily!</h1>
             <p className='hero__subtitle'>Streamline your car rental experience with our effortles booking proces.</p>
-            <CustomButton />
+            <CustomButton 
+             title="Explore Cars"
+             containerStyles="bg-primary-blue text-white rounded-full mt-10"
+             handleClick={handleScroll}
+            />
+        </div>
+        <div className='hero__image-container'>
+            <div className="hero__image">
+                <Image src="/hero.png" alt='hero' fill className='object-cantain'/>
+            </div>
+            <div className='hero__image-overlay' />
         </div>
     </div>
   )
